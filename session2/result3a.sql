fad4e2406a13a1a38eee6ac51d014d8bd1d53132
kimberlyh <hengst.kimberly@gmail.com>
2014-05-06 17:16:49 +0200

    VOlgens mij moest ik nog committen met 3a en semi 3b

---
+++ session2/result3a.sql
@@ -0,0 +1,21 @@
+select name,year,rating from movie where rating > 8.6 and rating < 9.1;
+
+Godfather, The|1972|9
+Shawshank Redemption, The|1994|9
+Schindler's List|1993|8.8
+Casablanca|1942|8.9
+Citizen Kane|1941|8.8
+American Beauty|1999|8.8
+Godfather: Part II, The|1974|8.8
+Star Wars|1977|8.8
+Shichinin no samurai|1954|9
+One Flew Over the Cuckoo's Nest|1975|8.7
+Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb|1964|8.8
+Rear Window|1954|8.7
+Usual Suspects, The|1995|8.7
+Paths of Glory|1957|8.8
+M|1931|8.7
+Almost Famous|2000|8.7
+General, The|1927|8.8
+Ladri di biciclette|1948|8.7
+Ikiru|1952|8.9
